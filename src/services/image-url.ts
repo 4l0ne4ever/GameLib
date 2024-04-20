0b0b861a61a521be505e4c110ab0d5710be340a2
Duong Thuyet <thuyetdoras.3@gmail.com>
2024-04-21 01:40:15 +0700

    fix bug

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,4 @@
-import noImage from '../assets/no-image-placeholder.webp';
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
 
 const getCroppedImageUrl = (url: string) => {
   if (!url) return noImage;
